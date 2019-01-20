695349a64b0678436fae7ac600fd0cd826fc5da5
TanishqSrivastava <yashu_comp@hotmail.com>
2019-01-20 19:12:50 +0530

    Post processing

--- 2.5DPlatformerGame/Assets/Scripts/NewBehaviourScript.cs
+++ 2.5DPlatformerGame/Assets/Scripts/NewBehaviourScript.cs
@@ -1,7 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 public class NewBehaviourScript : MonoBehaviour
 {
     private float moveInputX;
