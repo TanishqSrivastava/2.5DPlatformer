cb353bb43f9518583528619a7e26ddba66689b5e
TanishqSrivastava <yashu_comp@hotmail.com>
2019-04-03 23:33:04 +0530

    hjk

---
+++ 2.5DPlatformerGame/Assets/Scripts/CoinSystem.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoinSystem : MonoBehaviour
+{
+    public float coins;
+    public Text coinText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        coinText.text = coins.ToString();
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Coin") {
+            Destroy(other.gameObject);
+            coins++;
+
+        }
+    }
+}
